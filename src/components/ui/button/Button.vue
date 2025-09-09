583c6faa21daccb1cbfc1a4deea7ea831e458b2e
lmbsumatra <122442812+lmbsumatra@users.noreply.github.com>
2025-09-09 10:03:47 +0800

    :: fix import error

--- src/components/ui/button/Button.vue
+++ src/components/ui/button/Button.vue
@@ -3,8 +3,8 @@ import type { PrimitiveProps } from "reka-ui"
 import type { HTMLAttributes } from "vue"
 import type { ButtonVariants } from "."
 import { Primitive } from "reka-ui"
-import { cn } from "@/lib/utils"
 import { buttonVariants } from "."
+import { cn } from "../../../lib/utils"
 
 interface Props extends PrimitiveProps {
   variant?: ButtonVariants["variant"]
